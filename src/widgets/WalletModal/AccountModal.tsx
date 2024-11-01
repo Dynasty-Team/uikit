ebca36da5c466b301997a9c5356a8ef3bffa25e9
Nusantara <mumibiruinferno@gmail.com>
2024-11-01 22:21:46 +0700

    Update Link

--- src/widgets/WalletModal/AccountModal.tsx
+++ src/widgets/WalletModal/AccountModal.tsx
@@ -23,8 +23,8 @@ const AccountModal: React.FC<Props> = ({ account, logout, onDismiss = () => null
       {account}
     </Text>
     <Flex mb="32px">
-      <LinkExternal small href={`https://bscscan.com/address/${account}`} mr="16px">
-        View on BscScan
+      <LinkExternal small href={`https://dnyscan.io/address/${account}`} mr="16px">
+        View on DnyScan
       </LinkExternal>
       <CopyToClipboard toCopy={account}>Copy Address</CopyToClipboard>
     </Flex>
